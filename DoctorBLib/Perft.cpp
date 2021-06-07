6c34284f832799f9d94c3f5ff921a73e019719cc
Hans Blom <h.i.blom@tutanota.com>
2021-06-07 17:23:00 +0200

    restructure perft

--- DoctorBLib/Perft.cpp
+++ DoctorBLib/Perft.cpp
@@ -17,81 +17,54 @@ void Perft::setDepth(int value) {
 	depth_ = value;
 }
 
-uint64_t Perft::go() {
-	return count2(position_, depth_);
-}
-
-//Count function using recursion
-uint64_t Perft::count(const Position& position, const int remaining_depth) const {
-	MoveGenerator move_gen(position);
-
-	if (remaining_depth == 0)
-		return 1Ui64;
-
+uint64_t Perft::go() const {
 	uint64_t result = 0;
-	vector<Move> raw_moves;
-	Position new_position;
-	move_gen.generateMoves(raw_moves);
-	for (Move raw_move : raw_moves) {
-		new_position = Position(position);
-		new_position.applyMove(raw_move);
-		uint64_t cnt = count(new_position, remaining_depth - 1);
-		if (remaining_depth == depth_ && cnt > 0)
-			cout << raw_move.toString() << ": " << cnt << endl;
-		result += cnt;
-	}
-	return result;
-}
+	array<uint64_t, 128> results {};
 
-//Count function using a loop instead of recursion
-uint64_t Perft::count2(const Position& position, const int remaining_depth) const {
-	vector<uint64_t> results(128);
-	uint64_t result = 0;
-	
-	vector<Position> positions(depth_ + 1);
-	vector<vector<Move>> moves(depth_ + 1);
-	vector<int> move_indices(depth_ + 1, -1);
+	vector<State> states(depth_);
+	states[0].position = position_;
+	int max_depth = depth_ - 1;
 
-	int current_depth = remaining_depth;
-	positions[current_depth] = position;
-	
-	while (current_depth <= depth_) {
-		if (move_indices[current_depth] == -1) {
-			MoveGenerator move_gen(positions[current_depth]);
+	State& root_state = states[0];
+	int current_depth = 0;
+	while (current_depth >= 0) {
+		State& current_state = states[current_depth];
 
-			moves[current_depth].clear();
-			move_gen.generateMoves(moves[current_depth]);
-			if (current_depth == 1) {
-				if (depth_ == 1) {
-					for (int i = 0; i < moves[current_depth].size(); i++) {
-						results[i] = 1;
-					}
-					break;
-				}
-
-				results[move_indices[depth_]] += moves[current_depth].size();
-				current_depth++;
-				continue;
-
-			}
+		if (current_state.move_index == -1) {
+			current_state.generateMoves();
 		}
 
-		move_indices[current_depth]++;
-
-		if (move_indices[current_depth] >= moves[current_depth].size()) {
-			move_indices[current_depth] = -1;
-			current_depth++;
+		if (current_depth >= max_depth) {
+			if (current_depth > 0) {
+				results[root_state.move_index] += current_state.moves.size();
+			}
+			else {
+				for (int i = 0; i < current_state.moves.size(); i++) {
+					results[i] = 1;
+				}
+			}
+			current_state.move_index = -1;
+			current_depth--;
 			continue;
-		} 
+		}
 		
-		positions[current_depth - 1] = Position(positions[current_depth]);
-		positions[current_depth - 1].applyMove(moves[current_depth][move_indices[current_depth]]);
-		current_depth--;
+		current_state.move_index++;
+
+		if (current_state.move_index >= current_state.moves.size()) {
+			current_state.move_index = -1;
+			current_depth--;
+			continue;
+		}
+
+		current_depth++;
+		State& child_state = states[current_depth];
+		child_state.position = Position(current_state.position);
+		child_state.position.applyMove(current_state.moves[current_state.move_index]);
 	}
 
-	for (int i = 0; i < moves[depth_].size(); i++) {
+	for (int i = 0; i < states[0].moves.size(); i++) {
 		if (results[i]) {
-			cout << moves[depth_][i].toString() << ": " << results[i] << endl;
+			cout << states[0].moves[i].toString() << ": " << results[i] << endl;
 			result += results[i];
 		}
 	}
--- DoctorBLib/Perft.h
+++ DoctorBLib/Perft.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <vector>
 #include "Position.h"
+#include "MoveGenerator.h"
 
 class Perft
 {
@@ -7,11 +9,21 @@ public:
 	Perft(const Position& perft_position);
 	~Perft();
 	void setDepth(int value);
-	uint64_t go();
+	uint64_t go() const;
 private:
+	struct State {
+		Position position;
+		std::vector<Move> moves;
+		int move_index;
+		State() : position(Position()), moves(std::vector<Move>()), move_index(-1) {};
+		void generateMoves() {
+			moves.clear();
+			MoveGenerator move_gen(position);
+			move_gen.generateMoves(moves);
+		}
+	};
+
 	const Position& position_;
 	int depth_;
-	uint64_t count(const Position& position, const int remaining_depth) const;
-	uint64_t count2(const Position & position, const int remaining_depth) const;
 };
 
